d1953ca423f9bbeae4da95617c3d76d8cc209259
Sean Brown <37052749+SBSoftwareDev@users.noreply.github.com>
2026-07-04 12:07:30 -0700

    Minor adjustments

--- src/scripts/control.gd
+++ src/scripts/control.gd
@@ -1,8 +1,8 @@
 extends Control
 
 @onready var economy: Node = %Economy
-@onready var nectar_amount_label: RichTextLabel = $ColorRect/VBoxContainer/HBoxContainer/NectarAmountLabel
-@onready var bee_amount_label: RichTextLabel = $ColorRect/VBoxContainer/HBoxContainer/BeeAmountLabel
+@onready var nectar_amount_label: RichTextLabel = %NectarAmountLabel
+@onready var bee_amount_label: RichTextLabel = %BeeAmountLabel
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
