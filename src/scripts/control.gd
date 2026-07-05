4fd301bf2f97c90a4965558c6d6bf7241ff74a97
Sean Brown <37052749+SBSoftwareDev@users.noreply.github.com>
2026-07-05 02:53:21 -0700

    Added base UI for upgrades; buttons and such

--- src/scripts/control.gd
+++ src/scripts/control.gd
@@ -4,7 +4,11 @@ extends Control
 @onready var nectar_amount_label: RichTextLabel = %NectarAmountLabel
 @onready var bee_amount_label: RichTextLabel = %BeeAmountLabel
 
+func _ready() -> void:
+	nectar_amount_label.text = str("[img]res://assets/img/ui/nectar.png[/img] 0")
+	bee_amount_label.text = str("[img]res://src/scenes/worker_bee.tscn::AtlasTexture_xt75n[/img] 0")
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	nectar_amount_label.text = str("Nectar: ", economy.getNectarAmount())
-	bee_amount_label.text = str("Bees: ", economy.getBeeAmount())
+	nectar_amount_label.text = str("[img]res://assets/img/ui/nectar.png[/img] ", economy.getNectarAmount() )
+	bee_amount_label.text = str("[img]res://src/scenes/worker_bee.tscn::AtlasTexture_xt75n[/img] ", economy.getBeeAmount())
