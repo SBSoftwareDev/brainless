50859ce1ae6233ad3287eedd0f6c8e035a2eea5e
Sean Brown <37052749+SBSoftwareDev@users.noreply.github.com>
2026-07-10 04:29:24 -0700

    Hid queen from start. Started cell grouping

--- src/scripts/queen_bee.gd
+++ src/scripts/queen_bee.gd
@@ -2,10 +2,19 @@ extends Sprite2D
 
 const SPEED :float = 150.0
 
+var is_active :bool = false
+
 @onready var path_follow_2d: PathFollow2D = $"../"
 
+func _ready() -> void:
+	visible = false
+
+func activate_queen() -> void:
+	is_active = true
+	visible = true
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name.begins_with("Worker"):
+	if body.name.begins_with("Worker") && is_active:
 		body.leave_cell()
 
 
