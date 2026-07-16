9072d599a42f025bc03b63c0936b49c102038cd4
Sean Brown <37052749+SBSoftwareDev@users.noreply.github.com>
2026-07-15 17:27:03 -0700

    adjusted help menu size

--- src/scripts/help_menu.gd
+++ src/scripts/help_menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
 @onready var help_menu: Control = $"."
+@onready var panel: Panel = $"../UIRoot/Panel"
 
+func _ready() -> void:
+	help_menu.size.x -= panel.size.x
+	
 func _on_button_pressed() -> void:
 	help_menu.visible = false
